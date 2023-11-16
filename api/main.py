f64008519607f80eedf13e2e99ebd735e088eedb
igordantasgf <igordantasgf@gmail.com>
2023-11-15 22:12:28 -0300

    db connection + routes

--- api/main.py
+++ api/main.py
@@ -1,5 +1,67 @@
-from flask import Flask
+from flask import Flask, Response, request
+from flask_sqlalchemy import SQLAlchemy
+import json, os
+
+PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://default:{}@ep-autumn-dream-20888543.us-east-1.postgres.vercel-storage.com:5432/verceldb'.format(PASSWORD)
+db = SQLAlchemy(app)
+
+class Usuario(db.Model):
+    id = db.Column(db.String(30), primary_key= True)
+    autorizado = db.Column(db.String(1))
+
+    def to_json(self):
+        return {"id": self.id, "autorizado": self.autorizado}
+
+
+# Selecionar Tudo
+@app.route("/usuarios", methods=["GET"])
+def seleciona_usuarios():
+    usuarios_objetos = Usuario.query.all()
+    usuarios_json = [usuario.to_json() for usuario in usuarios_objetos]
+
+    return gera_response(200, "usuarios", usuarios_json)
+
+# Selecionar Individual
+@app.route("/usuario/<id>", methods=["GET"])
+def seleciona_usuario(id):
+    usuario_objeto = Usuario.query.filter_by(id=id).first()
+    usuario_json = usuario_objeto.to_json()
+
+    return gera_response(200, "usuario", usuario_json)
+
+# Atualizar
+@app.route("/change/<id>", methods=["PUT"])
+def atualiza_usuario(id):
+    usuario_objeto = Usuario.query.filter_by(id=id).first()
+    body = request.get_json()
+
+    try:
+        if('autorizado' in body):
+            usuario_objeto.autorizado = body['autorizado']
+        
+        db.session.add(usuario_objeto)
+        db.session.commit()
+        return gera_response(200, "usuario", usuario_objeto.to_json(), "Atualizado com sucesso")
+    except Exception as e:
+        print('Erro', e)
+        return gera_response(400, "usuario", {}, "Erro ao atualizar")
+
+# Deletar
+@app.route("/usuario/<id>", methods=["DELETE"])
+def deleta_usuario(id):
+    usuario_objeto = Usuario.query.filter_by(id=id).first()
+
+    try:
+        db.session.delete(usuario_objeto)
+        db.session.commit()
+        return gera_response(200, "usuario", usuario_objeto.to_json(), "Deletado com sucesso")
+    except Exception as e:
+        print('Erro', e)
+        return gera_response(400, "usuario", {}, "Erro ao deletar")
 
 @app.route("/")
 def home():
@@ -8,3 +70,14 @@ def home():
 @app.route("/login")
 def login():
     return "Login realizado"
+
+
+# JSON Return
+def gera_response(status, nome_do_conteudo, conteudo, mensagem=False):
+    body = {}
+    body[nome_do_conteudo] = conteudo
+
+    if(mensagem):
+        body["mensagem"] = mensagem
+
+    return Response(json.dumps(body), status=status, mimetype="application/json")
