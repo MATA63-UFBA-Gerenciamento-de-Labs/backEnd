03d252497bf9030b40572259860c805590d39e13
igordantasgf <igordantasgf@gmail.com>
2023-11-17 20:20:36 -0300

    import correction

--- main/aluno/views.py
+++ main/aluno/views.py
@@ -2,7 +2,7 @@ from . import aluno
 from flask import request
 from models import Usuario
 from main import db
-from response import gera_response
+from main.response import gera_response
 
 
 # Selecionar Tudo
