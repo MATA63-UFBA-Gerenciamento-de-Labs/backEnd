22db12b9803746334fffbc7dca67f1e458231383
igordantasgf <62483900+igordantasgf@users.noreply.github.com>
2023-11-21 16:21:01 -0300

    Update models.py

--- models.py
+++ models.py
@@ -4,8 +4,8 @@ from main import db
 class Usuario(db.Model):
     id = db.Column(db.String(9), primary_key= True)
     name = db.Column(db.String(50))
-    rfid_id_code = db.Column(db.String(11))
+    rf_id_code = db.Column(db.String(11))
     autorizado = db.Column(db.Boolean())
 
     def to_json(self):
-        return {"id": self.id, "name": self.name, "rfid_id_code": self.rfid_id_code, "autorizado": self.autorizado}
+        return {"id": self.id, "name": self.name, "rf_id_code": self.rf_id_code, "autorizado": self.autorizado}
