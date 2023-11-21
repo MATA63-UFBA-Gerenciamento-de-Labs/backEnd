64ad42ee550aa270ceea0b778f6a1d84386f3476
igordantasgf <62483900+igordantasgf@users.noreply.github.com>
2023-11-21 16:07:34 -0300

    Update views.py

--- main/aluno/views.py
+++ main/aluno/views.py
@@ -25,7 +25,7 @@ def seleciona_usuario(id):
 
 # Verificar autorização de RFID
 @aluno.route("/autorizado/<rfid_id_code>", methods=["GET"])
-def seleciona_usuario(rfid_id_code):
+def autorizacao_usuario(rfid_id_code):
     #usuario_objeto = Usuario.query.filter_by(rfid_id_code=rfid_id_code).first()
     usuario_objeto = Usuario.query.with_entities(Usuario.rfid_id_code, Usuario.autorizado).filter(Usuario.rfid_id_code == rfid_id_code).all()
     usuario_json = usuario_objeto.to_json()
