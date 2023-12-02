42b7bdea2ef1381f82acd952673585055457161f
Tiago Gordiano de Souza <tiagogordiano@gmail.com>
2023-12-02 08:22:11 -0300

    simplificar tests

--- test_rotas.py
+++ test_rotas.py
@@ -1,38 +1,33 @@
-import unittest
-from unittest.mock import MagicMock
-import json
 from main import app
-from models import Usuario
 
-class UsuarioRouteCase(unittest.TestCase):
-    def setUp(self):
-        self.app = app
-        self.client = self.app.test_client
-        self.db = MagicMock()
+USUARIO = {'id': 'teste', 'name': 'teste', 'rf_id_code': 'sample_id', 'autorizado': False}
 
-        self.app_context = self.app.app_context()
-        self.app_context.push()
+def test_seleciona_usuarios_status_code():
+    with app.test_client() as c:
+        response = c.get('/aluno/usuarios')
+        assert response.status_code == 200
 
-        self.db.create_all()
-        self.sample_usuario = Usuario(id="teste", name="teste", rf_id_code="sample_id", autorizado=False)
-        self.db.session.add(self.sample_usuario)
-        self.db.session.commit()
+def test_seleciona_usuarios_data():
+    with app.test_client() as c:
+        response = c.get('/aluno/usuarios')
+        data = response.get_json()
+        assert USUARIO in data["usuarios"]
 
-    def tearDown(self):
+def test_seleciona_usuario_existente():
+    with app.test_client() as c:
+        response = c.get('/aluno/usuarios')
+        data = response.get_json()
+        id_usuario = data["usuarios"][0]["id"]
+        response = c.get('/aluno/usuario/'+str(id_usuario))
+        data = response.get_json()
+        assert len(data) == 1
 
-        self.app_context.pop()
+def test_seleciona_usuario_inexistente():
+    with app.test_client() as c:
+        response = c.get('/aluno/usuario/inexistente')
+        assert response.status_code == 500
 
-    def test_seleciona_usuarios(self):
-        self.db.session.query.return_value.filter.return_value.all.return_value = [self.sample_usuario]
-
-        with self.app.app_context():
-            self.app.extensions['sqlalchemy'].db = self.db
-
-            response = self.client().get("/aluno/usuarios")
-            data = response.json 
-
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(len(data), 1)
-
-if __name__ == "__main__":
-    unittest.main()
+def test_deleta_usuario_inexistente():
+    with app.test_client() as c:
+        response = c.delete('/aluno/usuario/inexistente')
+        assert response.status_code == 400
